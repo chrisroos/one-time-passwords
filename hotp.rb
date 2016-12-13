1474b3238794e281955a9f6265fa6d7a01d3c8fd
Chris Roos <chris@seagul.co.uk>
2016-12-13 18:08:42 +0000

    Add a test to illustrate required format of counters
    The test data is based on the data in Appendix D of RFC 4226.
    
    I understand that the counter is an 8 byte hex string but I'm not yet
    sure how best to generate it from an integer.

--- hotp.rb
+++ hotp.rb
@@ -84,4 +84,24 @@ class HotpTest < Test::Unit::TestCase
     assert_equal expected_hex_string, Hotp.new.counter_to_hex(counter)
   end
 
+  def test_should_match_hmacs_from_test_data_in_rfc_4226
+    key = "12345678901234567890"
+    digest = OpenSSL::Digest.new('sha1')
+    [
+      ["\x00\x00\x00\x00\x00\x00\x00\x00", 'cc93cf18508d94934c64b65d8ba7667fb7cde4b0'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x01", '75a48a19d4cbe100644e8ac1397eea747a2d33ab'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x02", '0bacb7fa082fef30782211938bc1c5e70416ff44'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x03", '66c28227d03a2d5529262ff016a1e6ef76557ece'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x04", 'a904c900a64b35909874b33e61c5938a8e15ed1c'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x05", 'a37e783d7b7233c083d4f62926c7a25f238d0316'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x06", 'bc9cd28561042c83f219324d3c607256c03272ae'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x07", 'a4fb960c0bc06e1eabb804e5b397cdc4b45596fa'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x08", '1b3c89f65e6c9e883012052823443f048b4332db'],
+      ["\x00\x00\x00\x00\x00\x00\x00\x09", '1637409809a679dc698207310c8c7fc07290d9e5']
+    ].each do |(counter, expected_hmac)|
+      hmac = OpenSSL::HMAC.hexdigest(digest, key, counter)
+      assert_equal expected_hmac, hmac
+    end
+  end
+
 end
