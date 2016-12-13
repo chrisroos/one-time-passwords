31ffb22d82160273ce679b7a54838ed98fa27174
Chris Roos <chris@seagul.co.uk>
2016-12-13 20:17:00 +0000

    Convert hex byte strings to use `Array#pack`
    I **finally** worked out how to arrive at these byte string
    representations by using `Array#pack`. I think it was mostly a case of
    lots of trial and error allowing me to understand a little bit more
    which allowed me to continue my search of Google.

--- hotp.rb
+++ hotp.rb
@@ -88,18 +88,19 @@ class HotpTest < Test::Unit::TestCase
     key = "12345678901234567890"
     digest = OpenSSL::Digest.new('sha1')
     [
-      ["\x00\x00\x00\x00\x00\x00\x00\x00", 'cc93cf18508d94934c64b65d8ba7667fb7cde4b0'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x01", '75a48a19d4cbe100644e8ac1397eea747a2d33ab'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x02", '0bacb7fa082fef30782211938bc1c5e70416ff44'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x03", '66c28227d03a2d5529262ff016a1e6ef76557ece'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x04", 'a904c900a64b35909874b33e61c5938a8e15ed1c'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x05", 'a37e783d7b7233c083d4f62926c7a25f238d0316'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x06", 'bc9cd28561042c83f219324d3c607256c03272ae'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x07", 'a4fb960c0bc06e1eabb804e5b397cdc4b45596fa'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x08", '1b3c89f65e6c9e883012052823443f048b4332db'],
-      ["\x00\x00\x00\x00\x00\x00\x00\x09", '1637409809a679dc698207310c8c7fc07290d9e5']
+      [0, 'cc93cf18508d94934c64b65d8ba7667fb7cde4b0'],
+      [1, '75a48a19d4cbe100644e8ac1397eea747a2d33ab'],
+      [2, '0bacb7fa082fef30782211938bc1c5e70416ff44'],
+      [3, '66c28227d03a2d5529262ff016a1e6ef76557ece'],
+      [4, 'a904c900a64b35909874b33e61c5938a8e15ed1c'],
+      [5, 'a37e783d7b7233c083d4f62926c7a25f238d0316'],
+      [6, 'bc9cd28561042c83f219324d3c607256c03272ae'],
+      [7, 'a4fb960c0bc06e1eabb804e5b397cdc4b45596fa'],
+      [8, '1b3c89f65e6c9e883012052823443f048b4332db'],
+      [9, '1637409809a679dc698207310c8c7fc07290d9e5']
     ].each do |(counter, expected_hmac)|
-      hmac = OpenSSL::HMAC.hexdigest(digest, key, counter)
+      counter_as_64bit_unsigned_int = [counter].pack('Q>')
+      hmac = OpenSSL::HMAC.hexdigest(digest, key, counter_as_64bit_unsigned_int)
       assert_equal expected_hmac, hmac
     end
   end
