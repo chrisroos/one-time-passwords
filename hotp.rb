1ec35ac5d98327bfe5514fd1f676f44cd4608354
Chris Roos <chris@seagul.co.uk>
2016-12-14 14:16:10 +0000

    Add Hotp#log method
    So that I can toggle logging output in a single place.

--- hotp.rb
+++ hotp.rb
@@ -1,6 +1,10 @@
 require 'openssl'
 
 class Hotp
+  def log(*args)
+    p args
+  end
+
   def self.hotp(key, counter)
     hotp = new
     hmac = hotp.hmac(key, counter)
@@ -9,11 +13,11 @@ class Hotp
 
   # string = 20 byte binary string
   def dynamic_truncation(string)
-    p ['string', string]
-    p ['string as hex', string.unpack('H*')]
+    log 'string', string
+    log 'string as hex', string.unpack('H*')
 
     bytes_in_string = string.bytes.length
-    p ['bytes_in_string', bytes_in_string] # RFC says this should be 20 bytes
+    log 'bytes_in_string', bytes_in_string # RFC says this should be 20 bytes
 
     unless bytes_in_string == 20
       raise "Incorrect string size. Should be 20 bytes but is #{bytes_in_string}."
@@ -21,33 +25,33 @@ class Hotp
 
     # Let OffsetBits be the low-order 4 bits of String[19]
     last_byte = string.bytes.last
-    p ['last_byte', last_byte]
+    log 'last_byte', last_byte
     last_byte_as_hex = last_byte.to_s(16)
-    p ['last_byte_as_hex', last_byte_as_hex]
+    log 'last_byte_as_hex', last_byte_as_hex
     last_byte_as_binary = last_byte.to_s(2).rjust(8, '0')
-    p ['last_byte_as_binary', last_byte_as_binary]
+    log 'last_byte_as_binary', last_byte_as_binary
     offset_bits = last_byte_as_binary[4..7]
-    p ['offset_bits', offset_bits]
+    log 'offset_bits', offset_bits
 
     # Offset = StToNum(OffsetBits) // 0 <= OffSet <= 15
     offset = offset_bits.to_i(2)
-    p ['offset byte', offset]
+    log 'offset byte', offset
     # Let P = String[OffSet]...String[OffSet+3]
     pee_bytes = string.bytes[offset..offset + 3]
-    p ['pee_bytes', pee_bytes]
+    log 'pee_bytes', pee_bytes
     pee = pee_bytes.pack('CCCC')
-    p ['offset range', "#{offset}..#{offset + 3}"]
-    p ['pee', pee]
-    p ['pee as hex', pee.unpack('H*')]
+    log 'offset range', "#{offset}..#{offset + 3}"
+    log 'pee', pee
+    log 'pee as hex', pee.unpack('H*')
     bytes_in_pee = pee.bytes.length
-    p ['bytes_in_pee', bytes_in_pee]
+    log 'bytes_in_pee', bytes_in_pee
 
     # Return the Last 31 bits of P
     pee_in_binary = pee.bytes.map { |b| b.to_s(2).rjust(8, '0') }.join
-    p ['pee_in_binary', pee_in_binary]
+    log 'pee_in_binary', pee_in_binary
 
     last_31_bits_of_pee = pee_in_binary[1..31]
-    p ['last_31_bits_of_pee', last_31_bits_of_pee]
+    log 'last_31_bits_of_pee', last_31_bits_of_pee
     last_31_bits_of_pee
   end
 
@@ -58,19 +62,19 @@ class Hotp
 
   def hotp(hmac, digits)
     sbits = dynamic_truncation(hmac)
-    p ['sbits', sbits]
+    log 'sbits', sbits
 
-    p ['sbits.length', sbits.length]
+    log 'sbits.length', sbits.length
     unless sbits.length == 31
       raise "Incorrect string size. Should be 31 bits but is #{sbits.length}"
     end
 
     snum = sbits.to_i(2)
-    p ['snum', snum]
-    p ['snum as hex', snum.to_s(16)]
+    log 'snum', snum
+    log 'snum as hex', snum.to_s(16)
 
     dee = snum.modulo(10 ** digits)
-    p ['dee', dee]
+    log 'dee', dee
     dee
   end
 
