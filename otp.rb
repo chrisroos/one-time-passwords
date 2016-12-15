42092642284c5fd278e81f4c1aba9bb25d3a20eb
Chris Roos <chris@seagul.co.uk>
2016-12-15 10:16:27 +0000

    Add TODO item

--- otp.rb
+++ otp.rb
@@ -27,6 +27,7 @@ offset = hmac.bytes.last & 0x0f
 
 bytes = hmac.bytes[offset..offset + 3]
 bytes[0] = bytes[0] & 0x7f
+# TODO: Try packing this array using C and then unpacking using V. I think that's what James/Edward suggested at Show & Tell 27
 bytes_as_integer = bytes.map { |b| b.to_s(2).rjust(8, '0') }.join('').to_i(2)
 
 puts bytes_as_integer.modulo(10 ** digits)
